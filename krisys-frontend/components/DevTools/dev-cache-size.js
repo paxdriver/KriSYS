3d7f18d31bcb9202b7ebd4fe31cbc5d2a175f65e
Kristopher Driver <paxdriver@gmail.com>
2026-01-25 12:35:31 -0600

    helper scripts for monitoring size of localStorage cache.

---
+++ krisys-frontend/components/DevTools/dev-cache-size.js
@@ -0,0 +1,20 @@
+// GET SIZE OF LOCAL STORAGE
+(() => {
+	const bytes = Object.entries(localStorage).reduce(
+		(total, [k, v]) => total + k.length + v.length,
+		0
+	)
+	return {
+		bytes,
+		kb: (bytes / 1024).toFixed(2),
+		mb: (bytes / (1024 * 1024)).toFixed(2),
+	}
+})()
+
+// GET SIZE OF LOCAL STORAGE PER KEY
+Object.entries(localStorage)
+	.map(([k, v]) => ({
+		key: k,
+		bytes: k.length + v.length,
+	}))
+	.sort((a, b) => b.bytes - a.bytes)
